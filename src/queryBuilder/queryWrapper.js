fe5fa0092325a612598cdaa4dfb284cafc4d6678
Pavan Mahawar <pawanmahavir350@gmail.com>
2018-11-14 16:45:24 +0530

    Add queryBuilder dir

---
+++ src/queryBuilder/queryWrapper.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class QueryWrapper extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <p> Query Builders</p>
+        );
+    }
+}
+
+ export default QueryWrapper;
